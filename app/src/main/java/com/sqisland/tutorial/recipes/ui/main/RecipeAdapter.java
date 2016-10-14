3799dd56737ebdab4359539818b074edadf653e6
Chiu-Ki Chan <chiuki@users.noreply.github.com>
2016-10-14 15:03:07 -0600

    Add RecipeActivity

--- app/src/main/java/com/sqisland/tutorial/recipes/ui/main/RecipeAdapter.java
+++ app/src/main/java/com/sqisland/tutorial/recipes/ui/main/RecipeAdapter.java
@@ -1,5 +1,7 @@
 package com.sqisland.tutorial.recipes.ui.main;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -8,6 +10,7 @@ import android.view.ViewGroup;
 import com.sqisland.tutorial.recipes.R;
 import com.sqisland.tutorial.recipes.data.local.RecipeStore;
 import com.sqisland.tutorial.recipes.data.model.Recipe;
+import com.sqisland.tutorial.recipes.ui.recipe.RecipeActivity;
 
 public class RecipeAdapter extends RecyclerView.Adapter<RecipeViewHolder> {
   private final RecipeStore store;
@@ -27,6 +30,15 @@ public class RecipeAdapter extends RecyclerView.Adapter<RecipeViewHolder> {
   public void onBindViewHolder(final RecipeViewHolder holder, final int position) {
     final Recipe recipe = store.recipes.get(position);
     holder.textView.setText(recipe.title);
+    holder.textView.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View v) {
+        Context context = holder.textView.getContext();
+        Intent intent = new Intent(context, RecipeActivity.class);
+        intent.putExtra(RecipeActivity.KEY_ID, recipe.id);
+        context.startActivity(intent);
+      }
+    });
   }
 
   @Override
---
+++ app/src/main/java/com/sqisland/tutorial/recipes/ui/recipe/RecipeActivity.java
@@ -0,0 +1,36 @@
+package com.sqisland.tutorial.recipes.ui.recipe;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
+
+import com.sqisland.tutorial.recipes.R;
+import com.sqisland.tutorial.recipes.data.local.RecipeStore;
+import com.sqisland.tutorial.recipes.data.model.Recipe;
+
+public class RecipeActivity extends AppCompatActivity {
+  public static final String KEY_ID = "id";
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_recipe);
+
+    TextView titleView = (TextView) findViewById(R.id.title);
+    TextView descriptionView = (TextView) findViewById(R.id.description);
+
+    RecipeStore store = new RecipeStore(this, "recipes");
+    String id = getIntent().getStringExtra(KEY_ID);
+    final Recipe recipe = store.getRecipe(id);
+
+    if (recipe == null) {
+      titleView.setVisibility(View.GONE);
+      descriptionView.setText(R.string.recipe_not_found);
+      return;
+    }
+
+    titleView.setText(recipe.title);
+    descriptionView.setText(recipe.description);
+  }
+}
