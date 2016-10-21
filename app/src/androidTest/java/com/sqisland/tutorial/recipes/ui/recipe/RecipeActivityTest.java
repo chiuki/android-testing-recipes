8ef1579af83633399950b6c242bc2f881447997d
Chiu-Ki Chan <chiuki@users.noreply.github.com>
2016-10-21 12:05:18 -0600

    Add test which sets SharedPreferences state before launching activity

--- app/src/androidTest/java/com/sqisland/tutorial/recipes/ui/recipe/RecipeActivityTest.java
+++ app/src/androidTest/java/com/sqisland/tutorial/recipes/ui/recipe/RecipeActivityTest.java
@@ -1,10 +1,14 @@
 package com.sqisland.tutorial.recipes.ui.recipe;
 
 import android.content.Intent;
+import android.support.test.InstrumentationRegistry;
 import android.support.test.rule.ActivityTestRule;
 
 import com.sqisland.tutorial.recipes.R;
+import com.sqisland.tutorial.recipes.injection.InMemoryFavorites;
+import com.sqisland.tutorial.recipes.injection.TestRecipeApplication;
 
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 
@@ -18,12 +22,24 @@ import static android.support.test.espresso.matcher.ViewMatchers.withText;
 import static org.hamcrest.Matchers.not;
 
 public class RecipeActivityTest {
+  private static final String CARROTS_ID = "creamed_carrots";
+
   @Rule
   public ActivityTestRule<RecipeActivity> activityRule
       = new ActivityTestRule<>(
       RecipeActivity.class,
       true,    // initialTouchMode
       false);  // launchActivity
+
+  private InMemoryFavorites favorites;
+
+  @Before
+  public void clearFavorites() {
+    TestRecipeApplication app = (TestRecipeApplication)
+        InstrumentationRegistry.getTargetContext().getApplicationContext();
+    favorites = (InMemoryFavorites) app.getFavorites();
+    favorites.clear();
+  }
 
   @Test
   public void recipeNotFound() {
@@ -37,14 +53,28 @@ public class RecipeActivityTest {
 
   @Test
   public void clickToFavorite() {
-    Intent intent = new Intent();
-    intent.putExtra(RecipeActivity.KEY_ID, "creamed_carrots");
-    activityRule.launchActivity(intent);
+    launchRecipe(CARROTS_ID);
 
     onView(withId(R.id.title))
         .check(matches(withText("Creamed Carrots")))
         .check(matches(not(isSelected())))
         .perform(click())
         .check(matches(isSelected()));
+  }
+
+  @Test
+  public void alreadyFavorite() {
+    favorites.put(CARROTS_ID, true);
+
+    launchRecipe(CARROTS_ID);
+
+    onView(withId(R.id.title))
+        .check(matches(isSelected()));
+  }
+
+  private void launchRecipe(String id) {
+    Intent intent = new Intent();
+    intent.putExtra(RecipeActivity.KEY_ID, id);
+    activityRule.launchActivity(intent);
   }
 }
