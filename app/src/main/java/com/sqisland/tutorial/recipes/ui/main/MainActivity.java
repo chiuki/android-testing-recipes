3af092b5c42d53d8acf5c58acacaf6c81f1894bd
Chiu-Ki Chan <chiuki@users.noreply.github.com>
2016-11-02 12:57:46 -0600

    Add MainActivity

---
+++ app/src/main/java/com/sqisland/tutorial/recipes/ui/main/MainActivity.java
@@ -0,0 +1,14 @@
+package com.sqisland.tutorial.recipes.ui.main;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.sqisland.tutorial.recipes.R;
+
+public class MainActivity extends AppCompatActivity {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+  }
+}
