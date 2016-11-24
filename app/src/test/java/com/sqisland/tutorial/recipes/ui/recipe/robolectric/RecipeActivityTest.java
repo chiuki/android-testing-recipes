7d056723a0c8a31ea7fdcb2e7bb0fe4d0efc746d
Chiu-Ki Chan <chiuki@users.noreply.github.com>
2016-11-24 12:20:42 -0700

    Add recipeNotFound() for Robolectric testing

--- app/src/test/java/com/sqisland/tutorial/recipes/ui/recipe/robolectric/RecipeActivityTest.java
+++ app/src/test/java/com/sqisland/tutorial/recipes/ui/recipe/robolectric/RecipeActivityTest.java
@@ -1,12 +1,34 @@
 package com.sqisland.tutorial.recipes.ui.recipe.robolectric;
 
-import com.sqisland.tutorial.recipes.BuildConfig;
+import android.app.Activity;
+import android.view.View;
+import android.widget.TextView;
 
+import com.sqisland.tutorial.recipes.BuildConfig;
+import com.sqisland.tutorial.recipes.R;
+import com.sqisland.tutorial.recipes.ui.recipe.RecipeActivity;
+
+import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.robolectric.Robolectric;
 import org.robolectric.RobolectricTestRunner;
 import org.robolectric.annotation.Config;
+
+import static junit.framework.Assert.assertEquals;
 
 @RunWith(RobolectricTestRunner.class)
 @Config(constants = BuildConfig.class, sdk=22)
 public class RecipeActivityTest {
+  @Test
+  public void recipeNotFound() {
+    Activity activity = Robolectric.setupActivity(RecipeActivity.class);
+
+    assertEquals("Recipes", activity.getTitle());
+
+    TextView titleView = (TextView) activity.findViewById(R.id.title);
+    assertEquals(View.GONE, titleView.getVisibility());
+
+    TextView descriptionView = (TextView) activity.findViewById(R.id.description);
+    assertEquals("Recipe not found", descriptionView.getText());
+  }
 }
