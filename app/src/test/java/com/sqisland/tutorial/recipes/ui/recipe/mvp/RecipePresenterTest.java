59b3b05f86b6f4a8839b0b05b1b103f8cc4d2dc5
Chiu-Ki Chan <chiuki@users.noreply.github.com>
2016-11-24 12:28:38 -0700

    Add recipeNotFound() as MVP test, which is a JVM UI test

---
+++ app/src/test/java/com/sqisland/tutorial/recipes/ui/recipe/mvp/RecipePresenterTest.java
@@ -0,0 +1,32 @@
+package com.sqisland.tutorial.recipes.ui.recipe.mvp;
+
+import com.sqisland.tutorial.recipes.data.local.Favorites;
+import com.sqisland.tutorial.recipes.data.local.RecipeStore;
+import com.sqisland.tutorial.recipes.ui.recipe.RecipeContract;
+import com.sqisland.tutorial.recipes.ui.recipe.RecipePresenter;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mockito;
+
+public class RecipePresenterTest {
+  private RecipeStore store;
+  private Favorites favorites;
+  private RecipeContract.View view;
+  private RecipePresenter presenter;
+
+  @Before
+  public void setup() {
+    store = Mockito.mock(RecipeStore.class);
+    favorites = Mockito.mock(Favorites.class);
+    view = Mockito.mock(RecipeContract.View.class);
+    presenter = new RecipePresenter(store, favorites, view);
+  }
+
+  @Test
+  public void recipeNotFound() {
+    Mockito.when(store.getRecipe(Mockito.anyString())).thenReturn(null);
+    presenter.loadRecipe("no_such_recipe");
+    Mockito.verify(view, Mockito.times(1)).showRecipeNotFoundError();
+  }
+}
