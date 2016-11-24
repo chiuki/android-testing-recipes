17a391f812465b81cecf39a5b8f6f4008be65360
Chiu-Ki Chan <chiuki@users.noreply.github.com>
2016-11-24 12:29:40 -0700

    Add loadWaterAndFavorite() as MVP test

--- app/src/test/java/com/sqisland/tutorial/recipes/ui/recipe/mvp/RecipePresenterTest.java
+++ app/src/test/java/com/sqisland/tutorial/recipes/ui/recipe/mvp/RecipePresenterTest.java
@@ -2,12 +2,19 @@ package com.sqisland.tutorial.recipes.ui.recipe.mvp;
 
 import com.sqisland.tutorial.recipes.data.local.Favorites;
 import com.sqisland.tutorial.recipes.data.local.RecipeStore;
+import com.sqisland.tutorial.recipes.data.model.Recipe;
 import com.sqisland.tutorial.recipes.ui.recipe.RecipeContract;
 import com.sqisland.tutorial.recipes.ui.recipe.RecipePresenter;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.ArgumentCaptor;
 import org.mockito.Mockito;
+
+import java.io.InputStream;
+
+import static junit.framework.Assert.assertFalse;
+import static junit.framework.Assert.assertTrue;
 
 public class RecipePresenterTest {
   private RecipeStore store;
@@ -33,5 +40,22 @@ public class RecipePresenterTest {
   @Test(expected = IllegalStateException.class)
   public void toggleWithoutLoad() {
     presenter.toggleFavorite();
+  }
+
+  @Test
+  public void loadWaterAndFavorite() throws Exception {
+    InputStream stream = RecipePresenterTest.class.getResourceAsStream("/recipes/water.txt");
+    Recipe recipe = Recipe.readFromStream(stream);
+    Mockito.when(store.getRecipe(Mockito.anyString())).thenReturn(recipe);
+
+    Mockito.when(favorites.toggle(Mockito.anyString())).thenReturn(true);
+
+    presenter.loadRecipe("water");
+    presenter.toggleFavorite();
+
+    ArgumentCaptor<Boolean> captor = ArgumentCaptor.forClass(Boolean.class);
+    Mockito.verify(view, Mockito.times(2)).setFavorite(captor.capture());
+    assertFalse(captor.getAllValues().get(0));
+    assertTrue(captor.getAllValues().get(1));
   }
 }
